96ae31b47eae2ca8d76e9420c371f9a393890cb5
Aakash Kapoor <akashkapoor1356@gmail.com>
2022-07-04 18:05:06 +0530

    Adding new lines

--- todo.js
+++ todo.js
@@ -1,59 +1,64 @@
 function todo() {
     let ToDoInput = document.querySelector('.todo-input');
     let AddButton = document.querySelector('#button');
-    let UlAdded = document.querySelector('.ul-list');
-    let Start=document.createElement("Button");
-    let AddLi= document.createElement("Li");
-    let CheckBtn=document.createElement("button");
-    let ResetBtn = document.createElement("Button");
-   
-    if(ToDoInput.value===''){
+    var UlAdded = document.querySelector('.ul-list');
+    let Start = document.createElement("Button");
+    let AddLi = document.createElement("Li");
+    let CheckBtn = document.createElement("button");
+    let ResetBtn = document.createElement("button");
+    
+    let CounterData = document.querySelector('.counter');
+    if (ToDoInput.value === '') 
+    {
         window.alert("Please Enter Any Task");
     }
-    else
-    {
-        
+    else {
         AddLi.append(document.createTextNode(ToDoInput.value));
         UlAdded.append(AddLi);
-        UlAdded.appendChild(AddLi);
-        // var count = 0; 
-        // count++ ;
-        // let counte=document.querySelector('#counter');
-        // counte.innerHTML = count;
+        ToDoInput.value = "";
     }
-
-        let cut = document.createElement("Button");
-        cut.append(document.createTextNode("remove"));
-        // cut.style.marginLeft = '10px';
-        AddLi.append(cut);
-        cut.addEventListener('click', function () {
+    let cut = document.createElement("Button");
+    cut.append(document.createTextNode("remove"));
+    AddLi.append(cut);
+    cut.addEventListener('click', function () {
         AddLi.remove();
         CheckBtn.append(document.createTextNode("Done"));
         AddLi.append(CheckBtn);
         CheckBtn.style.display = 'none';
-        ResetBtn.style.display = 'none';
+        ResetBtn.append(document.createTextNode("reset"));
+        AddLi.append(ResetBtn);
+        ResetBtn.style.display= 'none'; 
     });
-    
 
     Start.append(document.createTextNode("Start"));
-    // Start.style.marginLeft = '40px';
     AddLi.append(Start);
-    
-    Start.addEventListener('click',function () {
-        let InproUl=document.querySelector(".inprogress");
+    Start.addEventListener('click', function () {
+        let InproUl = document.querySelector(".inprogress");
         InproUl.append(AddLi);
         Start.style.display = 'none';
         AddLi.append(CheckBtn);
-        
-        // CheckBtn.innerHTML = "&check;";
-        // InproUl.appendChild(CheckBtn);
     });
     CheckBtn.append(document.createTextNode("Done"));
-    // AddLi.append(CheckBtn);
-    CheckBtn.addEventListener('click',function (){
-        let CompleteUl=document.querySelector('.complete');
+    CheckBtn.addEventListener('click', function () {
+        let CompleteUl = document.querySelector('.complete');
         CompleteUl.append(AddLi);
+        AddLi.append(ResetBtn);
         CheckBtn.style.display = 'none';
+    
     });
+   
+    // ResetBtn.append(document.createTextNode("reset"));
+    
+    // ResetBtn.addEventListener('click',function(){
+    //     // UlAdded.append(AddLi);
+    //     // Start.append(document.createTextNode("Start"));
+    //     // Start.append(AddLi);
+    //     AddLi.append(ToDoInput.value);
+    //     // Start.append(document.createTextNode("Start"));
+    //     // Start.append(AddLi);
+    //     UlAdded.append(AddLi);
+       
+    //     ResetBtn.style.display = 'none'; 
+    // });
+        
 };
-
